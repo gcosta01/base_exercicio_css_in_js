b93ad8f9d70b4db168d848ab5e7561e5078e7f15
Gabriel Costa <bob_gbl@hotmail.com>
2024-01-31 16:42:34 -0300

    first commit

---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,28 @@
+import { FormEvent, useState } from 'react'
+
+import { Forumalrio, Botao, Campo } from './styles'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Forumalrio onSubmit={aoEnviarForm}>
+      <Campo
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <Botao type="submit">Pesquisar</Botao>
+    </Forumalrio>
+  )
+}
+export default FormVagas
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,11 @@
+import { Forumalario, Title } from './styles'
+
+const Hero = () => (
+  <Forumalario>
+    <div>
+      <Title>As melhores vagas para tecnologia, design e artes visuais.</Title>
+    </div>
+  </Forumalario>
+)
+
+export default Hero
